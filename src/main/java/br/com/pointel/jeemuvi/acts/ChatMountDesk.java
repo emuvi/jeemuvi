d18fe834568b07590af6aa4a93317f4c720536b0
Éverton M. Vieira <emuvi@outlook.com.br>
2024-10-11 19:06:16 -0300

    updates

--- src/main/java/br/com/pointel/jeemuvi/acts/ChatMountDesk.java
+++ src/main/java/br/com/pointel/jeemuvi/acts/ChatMountDesk.java
@@ -3,6 +3,8 @@ package br.com.pointel.jeemuvi.acts;
 import br.com.pointel.jeemuvi.wizes.WizChats;
 import br.com.pointel.jeemuvi.wizes.WizSwing;
 import java.awt.event.InputEvent;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.DefaultComboBoxModel;
 
 /**
@@ -12,13 +14,13 @@ import javax.swing.DefaultComboBoxModel;
 public class ChatMountDesk extends javax.swing.JFrame {
 
     private final DefaultComboBoxModel<String> modelChats = new DefaultComboBoxModel<>();
-    
+
     public ChatMountDesk() {
         initComponents();
         initFrame();
     }
-    
-    private void initFrame() { 
+
+    private void initFrame() {
         WizChats.loadNames(modelChats);
         WizSwing.initFrame(this);
         WizSwing.initEscaper(this);
@@ -28,20 +30,30 @@ public class ChatMountDesk extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonOpenOrRefresh = new javax.swing.JButton();
         fieldChat = new javax.swing.JComboBox<>();
         buttonGetChat = new javax.swing.JButton();
+        buttonInsertHere = new javax.swing.JButton();
         buttonEditChat = new javax.swing.JButton();
         buttonGetFromClipboard = new javax.swing.JButton();
+        buttonPutOnBuffer = new javax.swing.JButton();
+        labelBuffer = new javax.swing.JLabel();
         buttonPutOnClipboard = new javax.swing.JButton();
-        buttonOpenOrRefresh = new javax.swing.JButton();
         buttonMount = new javax.swing.JButton();
         scrollMount = new javax.swing.JScrollPane();
         fieldMount = new javax.swing.JTextArea();
-        buttonInsertHere = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("ChatMount");
         setName("ChatMountDesk"); // NOI18N
+
+        buttonOpenOrRefresh.setText("#");
+        buttonOpenOrRefresh.setToolTipText("Open or Refresh Chats");
+        buttonOpenOrRefresh.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonOpenOrRefreshActionPerformed(evt);
+            }
+        });
 
         fieldChat.setModel(modelChats);
 
@@ -50,6 +62,14 @@ public class ChatMountDesk extends javax.swing.JFrame {
         buttonGetChat.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 buttonGetChatActionPerformed(evt);
+            }
+        });
+
+        buttonInsertHere.setText("|");
+        buttonInsertHere.setToolTipText("Put Insert Here");
+        buttonInsertHere.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonInsertHereActionPerformed(evt);
             }
         });
 
@@ -69,19 +89,21 @@ public class ChatMountDesk extends javax.swing.JFrame {
                 }
             });
 
+            buttonPutOnBuffer.setText("+");
+            buttonPutOnBuffer.setToolTipText("Put on Buffer");
+            buttonPutOnBuffer.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    buttonPutOnBufferActionPerformed(evt);
+                }
+            });
+
+            labelBuffer.setText("0");
+
             buttonPutOnClipboard.setText("/");
             buttonPutOnClipboard.setToolTipText("Put on Clipboard");
             buttonPutOnClipboard.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
                     buttonPutOnClipboardActionPerformed(evt);
-                }
-            });
-
-            buttonOpenOrRefresh.setText("#");
-            buttonOpenOrRefresh.setToolTipText("Open or Refresh Chats");
-            buttonOpenOrRefresh.addActionListener(new java.awt.event.ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent evt) {
-                    buttonOpenOrRefreshActionPerformed(evt);
                 }
             });
 
@@ -96,14 +118,6 @@ public class ChatMountDesk extends javax.swing.JFrame {
             fieldMount.setColumns(20);
             fieldMount.setRows(5);
             scrollMount.setViewportView(fieldMount);
-
-            buttonInsertHere.setText("|");
-            buttonInsertHere.setToolTipText("Put Insert Here");
-            buttonInsertHere.addActionListener(new java.awt.event.ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent evt) {
-                    buttonInsertHereActionPerformed(evt);
-                }
-            });
 
             javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
             getContentPane().setLayout(layout);
@@ -126,10 +140,14 @@ public class ChatMountDesk extends javax.swing.JFrame {
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(buttonGetFromClipboard)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(buttonPutOnBuffer)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(labelBuffer)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(buttonPutOnClipboard)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(buttonMount)
-                            .addGap(0, 81, Short.MAX_VALUE)))
+                            .addGap(0, 0, Short.MAX_VALUE)))
                     .addContainerGap())
             );
             layout.setVerticalGroup(
@@ -144,7 +162,9 @@ public class ChatMountDesk extends javax.swing.JFrame {
                         .addComponent(buttonPutOnClipboard)
                         .addComponent(buttonOpenOrRefresh)
                         .addComponent(buttonMount)
-                        .addComponent(buttonInsertHere))
+                        .addComponent(buttonInsertHere)
+                        .addComponent(buttonPutOnBuffer)
+                        .addComponent(labelBuffer))
                     .addGap(18, 18, 18)
                     .addComponent(scrollMount, javax.swing.GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
                     .addContainerGap())
@@ -167,11 +187,15 @@ public class ChatMountDesk extends javax.swing.JFrame {
 
     private void buttonGetChatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGetChatActionPerformed
         try {
-            fieldMount.setText(WizChats.load(getChatName()));
+            getChat();
         } catch (Exception e) {
             WizSwing.showError(e);
         }
     }//GEN-LAST:event_buttonGetChatActionPerformed
+
+    private void getChat() throws Exception {
+        fieldMount.setText(WizChats.load(getChatName()));
+    }
 
     private void buttonEditChatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonEditChatActionPerformed
         try {
@@ -184,40 +208,79 @@ public class ChatMountDesk extends javax.swing.JFrame {
     private String getChatName() {
         return fieldChat.getSelectedItem().toString();
     }
-    
+
     private void buttonGetFromClipboardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGetFromClipboardActionPerformed
         try {
-            var clipboard = WizSwing.getStringOnClipboard();
-            var source = fieldMount.getText().trim();
-            if (source.contains("<<INSERT_HERE>>")) {
-                source = source.replace("<<INSERT_HERE>>", clipboard);
-            } else {
-                source += "\n\n" + clipboard;
-            }
-            fieldMount.setText(source);
+            getFromClipboard();
         } catch (Exception e) {
             WizSwing.showError(e);
         }
     }//GEN-LAST:event_buttonGetFromClipboardActionPerformed
 
+    private void getFromClipboard() throws Exception {
+        var clipboard = WizSwing.getStringOnClipboard();
+        var source = fieldMount.getText().trim();
+        if (source.contains("<<INSERT_HERE>>")) {
+            source = source.replace("<<INSERT_HERE>>", clipboard);
+        } else {
+            source += "\n\n" + clipboard;
+        }
+        fieldMount.setText(source);
+    }
+
     private void buttonPutOnClipboardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonPutOnClipboardActionPerformed
         try {
-            var source = fieldMount.getText().trim();
-            WizSwing.putStringOnClipboard(source);
+            putOnClipboard();
         } catch (Exception e) {
             WizSwing.showError(e);
         }
     }//GEN-LAST:event_buttonPutOnClipboardActionPerformed
 
+    private void putOnClipboard() {
+        var source = fieldMount.getText().trim();
+        WizSwing.putStringOnClipboard(source);
+    }
+
     private void buttonMountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonMountActionPerformed
-        buttonGetChatActionPerformed(evt);
-        buttonGetFromClipboardActionPerformed(evt);
-        buttonPutOnClipboardActionPerformed(evt);
+        try {
+            getChat();
+            mountWithBuffer();
+            getFromClipboard();
+            putOnClipboard();
+        } catch (Exception e) {
+            WizSwing.showError(e);
+        }
     }//GEN-LAST:event_buttonMountActionPerformed
+
+    private void mountWithBuffer() throws Exception {
+        if (!buffer.isEmpty()) {
+            var onClipboard = WizSwing.getStringOnClipboard();
+            if (onClipboard != null) {
+                buffer.add(onClipboard);
+            }
+            WizSwing.putStringOnClipboard(String.join("\n", buffer));
+            buffer.clear();
+        }
+        labelBuffer.setText(String.valueOf(buffer.size()));
+    }
 
     private void buttonInsertHereActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonInsertHereActionPerformed
         fieldMount.replaceSelection("<<INSERT_HERE>>");
     }//GEN-LAST:event_buttonInsertHereActionPerformed
+
+    private final List<String> buffer = new ArrayList<String>();
+
+    private void buttonPutOnBufferActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonPutOnBufferActionPerformed
+        try {
+            var onClipboard = WizSwing.getStringOnClipboard();
+            if (onClipboard != null) {
+                buffer.add(onClipboard);
+            }
+            labelBuffer.setText(String.valueOf(buffer.size()));
+        } catch (Exception e) {
+            WizSwing.showError(e);
+        }
+    }//GEN-LAST:event_buttonPutOnBufferActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonEditChat;
@@ -226,9 +289,11 @@ public class ChatMountDesk extends javax.swing.JFrame {
     private javax.swing.JButton buttonInsertHere;
     private javax.swing.JButton buttonMount;
     private javax.swing.JButton buttonOpenOrRefresh;
+    private javax.swing.JButton buttonPutOnBuffer;
     private javax.swing.JButton buttonPutOnClipboard;
     private javax.swing.JComboBox<String> fieldChat;
     private javax.swing.JTextArea fieldMount;
+    private javax.swing.JLabel labelBuffer;
     private javax.swing.JScrollPane scrollMount;
     // End of variables declaration//GEN-END:variables
 }
