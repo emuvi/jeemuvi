3369b4f789b324c9cff16228a9a06846f0bd61a8
Éverton M. Vieira <emuvi@outlook.com.br>
2024-10-20 15:08:27 -0300

    updates

--- src/main/java/br/com/pointel/jeemuvi/gears/CharsSections.java
+++ src/main/java/br/com/pointel/jeemuvi/gears/CharsSections.java
@@ -26,7 +26,7 @@ public class CharsSections {
     }
     
     public Map<String, List<String>> read(TextHistory history) throws Exception {
-        var source = Files.readString(file.toPath(), StandardCharsets.UTF_8);
+        var source = Files.readString(file.toPath(), StandardCharsets.UTF_8).trim();
         if (history != null) {
             history.push(file, source);
         }
@@ -38,7 +38,20 @@ public class CharsSections {
         for (int i = 0; i < lines.length; i++) {
             var actualLine = lines[i];
             var nextLine = i < lines.length - 1 ? lines[i + 1] : "";
-            if (actualLine.startsWith("# ") && "---".equals(nextLine)) {
+            var plusLine = i < lines.length - 2 ? lines[i + 2] : "";
+            if ("---".equals(actualLine) && nextLine.startsWith("# ")) {
+                actualName = nextLine.substring(2);
+                if (result.containsKey(actualName)) {
+                    actualList = result.get(actualName);
+                } else {
+                    actualList = new ArrayList<>();
+                    result.put(actualName, actualList);
+                }
+                i += 1;
+                if ("---".equals(plusLine)) {
+                    i += 1;
+                }
+            } else if (actualLine.startsWith("# ") && "---".equals(nextLine)) {
                 actualName = actualLine.substring(2);
                 if (result.containsKey(actualName)) {
                     actualList = result.get(actualName);
@@ -46,7 +59,7 @@ public class CharsSections {
                     actualList = new ArrayList<>();
                     result.put(actualName, actualList);
                 }
-                i++;
+                i += 1;
             } else {
                 actualList.add(actualLine);
             }
@@ -60,6 +73,7 @@ public class CharsSections {
 
     public void write(Map<String, List<String>> source, TextHistory history) throws Exception {
         var builder = new StringBuilder();
+        builder.append("\n");
         var emptyNameSectionLines = source.get("");
         if (!emptyNameSectionLines.isEmpty()) {
             putLines(builder, emptyNameSectionLines, false);
@@ -69,7 +83,7 @@ public class CharsSections {
                 continue;
             }
             var lines = source.get(sectionName);
-            builder.append("\n---\n");
+            builder.append("---\n");
             builder.append("# ");
             builder.append(sectionName);
             builder.append("\n---\n");
