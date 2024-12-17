d1e82f4f6064d056788efbd2b158cb048eaeb72b
Éverton M. Vieira <emuvi@outlook.com.br>
2024-12-16 21:20:37 -0300

    adds log on start

--- src/main/java/br/com/pointel/jeemuvi/Jeemuvi.java
+++ src/main/java/br/com/pointel/jeemuvi/Jeemuvi.java
@@ -1,12 +1,18 @@
 package br.com.pointel.jeemuvi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author emuvi
  */
 public class Jeemuvi {
 
+    private static final Logger logger = LoggerFactory.getLogger(Jeemuvi.class);
+
     public static void main(String[] args) {
+        logger.info("Started");
         JeemuviDesk.start(args);
     }
     
